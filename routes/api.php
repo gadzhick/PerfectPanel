b07fbbb659e05dc8d9222dc4e567d25b5ac09d40
gadzhick <dimagyzhdian@gmail.com>
2025-01-27 21:40:32 +0300

    add routing

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,18 @@
 <?php
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+Route::match([Request::METHOD_GET, Request::METHOD_POST], 'v1', [ApiController::class, 'handle']);
+
+Route::any('{any?}', static function () {
+    return response(
+        [
+            "status" => "error",
+            "code" => Response::HTTP_NOT_FOUND,
+            "message" => "Unknown route"
+        ],
+        Response::HTTP_NOT_FOUND);
+})->where('any', '^.*$');
